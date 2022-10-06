bba788a1503bfa192264183cb5cc430b185e3100
Rachel Braga <rmbraga@me.com>
2022-10-06 16:00:44 -0300

    feat| models and migrations

--- dados/models.py
+++ dados/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class transaction(models.TextChoices):
+    D = "Débito"
+    B = "Boleto"
+    F = "Financiamento"
+    C = "Crédito"
+    RE = "Recebimento Empréstimo"
+    V = "Vendas"
+    RTED = "Recebimento TED"
+    RDOC = "Recebimento DOC"
+    A = "Aluguel"
+    DEFAULT = "Nada"
+
+
+class Document(models.Model):
+    tipo = models.CharField(max_length=2)
+    data = models.CharField(max_length=8)
+    valor = models.CharField(max_length=10)
+    cpf = models.CharField(max_length=11)
+    cartao = models.CharField(max_length=12)
+    hora = models.CharField(max_length=6)
+    dono_da_loja = models.CharField(max_length=14)
+    nome_loja = models.CharField(max_length=19)
