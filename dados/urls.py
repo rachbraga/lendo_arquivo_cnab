d08d0e87a5e68b4a0ff67fcc33a32850dc031b80
Rachel Braga <rmbraga@me.com>
2022-10-10 21:33:26 -0300

    feat | listing and subtotal

--- dados/urls.py
+++ dados/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     re_path(r"^upload/(?P<filename>[^/]+)$", views.FileUploadView.as_view()),
+    path("dados/", views.FileUploadView.as_view()),
 ]
--- dados/views.py
+++ dados/views.py
@@ -1,10 +1,12 @@
+from platform import libc_ver
+
 from rest_framework import status
 from rest_framework.parsers import FileUploadParser, JSONParser
 from rest_framework.views import APIView, Response
 
-from .serializers import DadosSerializers
+from dados.models import Dados
 
-# Create your views here.
+from .serializers import DadosSerializers
 
 
 class FileUploadView(APIView):
@@ -34,3 +36,64 @@ class FileUploadView(APIView):
             data_cnab = [serializer.data] + data_cnab
 
         return Response(data=data_cnab, status=status.HTTP_201_CREATED)
+
+    def get(self, request):
+        dados = Dados.objects.all()
+
+        saldo_em_conta = 0
+        soma = 0
+        menos = 0
+
+        lista_lojas_debito = []
+        lista_lojas_boleto = []
+        lista_lojas_financiamento = []
+        lista_lojas_credito = []
+        lista_lojas_re = []
+        lista_lojas_vendas = []
+        lista_lojas_rt = []
+        lista_lojas_rd = []
+        lista_lojas_aluguel = []
+        lista_lojas = []
+        loja = {}
+        for item in dados:
+            loja = dict(
+                loja=item.nome_loja, tipo=item.tipo, valor=(int(item.valor) / 100.00)
+            )
+            lista_lojas = lista_lojas + [loja]
+            if (
+                item.tipo == "1"
+                or item.tipo == "4"
+                or item.tipo == "5"
+                or item.tipo == "6"
+                or item.tipo == "7"
+                or item.tipo == "8"
+            ):
+                soma += int(item.valor) / 100.00
+
+            else:
+                menos -= int(item.valor) / 100.00
+        ##FUTURAMENTE SEPARAR POR SESSÕES:
+        # if loja["tipo"] == "1":
+        #     debito += [loja["valor"]]
+        #     lista_lojas_debito = lista_lojas_debito + [loja]
+        # if loja["tipo"] == "2":
+        #     lista_lojas_boleto = lista_lojas_boleto + [loja]
+        # if loja["tipo"] == "3":
+        #     lista_lojas_financiamento = lista_lojas_financiamento + [loja]
+        # if loja["tipo"] == "4":
+        #     lista_lojas_credito = lista_lojas_credito + [loja]
+        # if loja["tipo"] == "5":
+        #     lista_lojas_re = lista_lojas_re + [loja]
+        # if loja["tipo"] == "6":
+        #     lista_lojas_vendas = lista_lojas_vendas + [loja]
+        # if loja["tipo"] == "7":
+        #     lista_lojas_rt = lista_lojas_rt + [loja]
+        # if loja["tipo"] == "8":
+        #     lista_lojas_rd = lista_lojas_rd + [loja]
+        # if loja["tipo"] == "9":
+        #     lista_lojas_aluguel = lista_lojas_aluguel + [loja]
+
+        #
+
+        saldo_em_conta = soma + menos
+        return Response({"Transações": lista_lojas, "SALDO EM CONTA": saldo_em_conta})
